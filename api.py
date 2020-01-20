02bd504894d3bbba2080c2913daf2dd186ff25b4
Benjamin AKHRAS <benjamin.akhras@bmlv.gv.at>
2020-01-20 14:19:26 +0100

    switch to cert based auth

--- api.py
+++ api.py
@@ -1,17 +1,20 @@
-#!/usr/bin/python3.8
+#!/usr/bin/python2ve
 # coding=utf-8
 """
 Icinga API Client
 """
 import requests
 import pprint
+import socket
 
-API_URI = "https://icinga:5665/v1"
+FQDN = socket.getfqdn()
+API_URI = "https://{0}:5665/v1".format(FQDN)
 API_AUTH = ('root', 'icinga')
+API_CERT = ("/var/lib/icinga2/certs/{0}.crt".format(FQDN), "/var/lib/icinga2/certs/{0}.key".format(FQDN))
 
 
 def get_status(object: str):
-    r = requests.get(API_URI + '/objects/' + object, auth=API_AUTH)
+    r = requests.get(API_URI + '/objects/' + object, cert=API_CERT)
     result_json = r.json()['results']
     return_dict = {}
     for result in result_json:
@@ -26,3 +29,5 @@ def get_status(object: str):
 if __name__ == "__main__":
     pprint.pprint(get_status('hosts'), width=200)
     pprint.pprint(get_status('services'), width=200)
+    # /v1/actions/reschedule-check -d '{ "type": "Service", "filter": "service.name==\"$1\"" }'
+
